d7b341e7dd83f989535cc4611a62cc79269368f6
UMI64 <949426508@qq.com>
2018-04-01 00:43:55 +0800

    添加项目文件。

---
+++ BPorject/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BPorject
+{
+    [Activity(Label = "BPorject")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            int Count=0;
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.button);
+            button.Click += (sender, e) =>
+            {
+                Count += 1;
+                button.Text = "你点了" + Count.ToString() + "下";
+            };
+        }
+
+
+    }
+}
+
